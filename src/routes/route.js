1e13049ff570064a69cdc2807c8f5691049793fb
omkarb77 <bomkar1506@gmail.com>
2022-08-10 20:45:50 +0530

    hiiiiiii

--- src/routes/route.js
+++ src/routes/route.js
@@ -54,7 +54,7 @@ router.post("/test-post-3", function(req, res) {
     // let id = req.body.user
     // let pwd= req.body.password
 
-    // console.log( id , pwd)
+    //console.log( id , pwd)
 
     console.log( req.body )
 
@@ -69,5 +69,53 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+
+let players= 
+[
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+            "soccer"
+        ]
+    }
+]
+router.post('/players', function (req, res) {
+let newPlayer  = req.body;
+let newPlayersName =newPlayer.name
+let isNameRepeated = false
+    //LOGIC WILL COME HERE
+for(let i=0; i<players.length; i++){
+    if (players[i].name == newPlayersName){
+        isNameRepeated= true;
+       break;}}
+    if(isNameRepeated){res.send("This player was already added !")}
+    else{ players.push(newPlayer)
+    res.send(players)
+    }
+})
+    
+//    res.send(  { data: players , status: true }  )
+//})
 
 module.exports = router;
